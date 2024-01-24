ef041acdf1e667963697aa95f4a0f4ea9a8f8169
timothy-williams <timothy.r.williams@outlook.com>
2024-01-24 17:10:41 -0500

    init

---
+++ test/new-twilliams-resume.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NewTwilliamsResume from '../lib/new-twilliams-resume-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/new-twilliams-resume-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NewTwilliamsResume.NewTwilliamsResumeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
