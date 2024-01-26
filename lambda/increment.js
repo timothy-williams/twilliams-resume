cf96bd1332062926557715d69903af3ffea74510
timothy-williams <timothy.r.williams@outlook.com>
2024-01-26 15:40:46 -0500

    Create increment.js
    Create Lambda function that increments VisitorCount by 1 in DynamoDB table

---
+++ lambda/increment.js
@@ -0,0 +1,14 @@
+import { DynamoDB } from 'aws-sdk';
+const dynamoDB = new DynamoDB();
+
+async function getCurrentCount() {
+    
+}
+  
+async function updateCount(newCount) {
+    
+}
+
+export async function incrementHandler(event, context) {
+  
+}
