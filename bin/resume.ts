da41074a1d252982146446f8fb4e6180e247d151
timothy-williams <timothy.r.williams@outlook.com>
2024-01-30 12:54:55 -0500

    Remove ACM secret, add account variables

--- bin/resume.ts
+++ bin/resume.ts
@@ -5,18 +5,8 @@ import { ResumeStack } from "../lib/resume-stack";
 
 const app = new cdk.App();
 new ResumeStack(app, "ResumeStack", {
-  ACMSecret: "Resume-ACM-Certificate-ARN",
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {
+    account: app.node.tryGetContext('accountId'),
+    region: 'us-east-1',
+  }, // need to make sure these variables are configured properly
 });
