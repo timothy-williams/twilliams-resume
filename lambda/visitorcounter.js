1f3604c07516a13130295d915f961cab1c72e036
timothy-williams <timothy.r.williams@outlook.com>
2024-02-02 16:19:37 -0500

    Fixed DynamoDB import

--- lambda/visitorcounter.js
+++ lambda/visitorcounter.js
@@ -1,17 +1,19 @@
-import { DynamoDB } from ("aws-sdk");
+const { DynamoDB } = require("aws-sdk");
 
-exports.handler = async function(event) {
+exports.handler = async function (event) {
   const dynamo = new DynamoDB();
   console.log("Request:", JSON.stringify(event, undefined, 2));
 
   // Increment VisitorCount by 1
-  const eventData = await dynamo.updateItem({
-    TableName: process.env.TABLE_NAME,
-    Key: { path: { N: "VisitorCount" } },
-    UpdateExpression: 'ADD Hits :incr',
-    ExpressionAttributeValues: { ':incr': { N: '1' } },
-    ReturnValues: 'UPDATED_NEW',
-  }).promise();
+  const eventData = await dynamo
+    .updateItem({
+      TableName: process.env.TABLE_NAME,
+      Key: { path: { N: "VisitorCount" } },
+      UpdateExpression: "ADD Hits :incr",
+      ExpressionAttributeValues: { ":incr": { N: "1" } },
+      ReturnValues: "UPDATED_NEW",
+    })
+    .promise();
 
   console.log("Event data:", JSON.stringify(eventData, undefined, 2));
 
@@ -19,6 +21,7 @@ exports.handler = async function(event) {
   const newCount = eventData.Attributes.Hits.N;
   return {
     statusCode: 200,
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ newCount }),
   };
 };
